8a60acfff9cf0ffd45280b0678199f78d58491d7
hayato hasegawa <hase1225hayato@gmail.com>
2020-05-16 23:38:50 +0900

    implement simple html

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function component() {
+  const element = document.createElement('div');
+  const array = ["Hello", "WebPack!", "hase"]
+  element.innerHTML = _.join(array, '〜');
+  return element;
+}
+
+document.body.appendChild(component());
