3b51a97289d48aff3c3d7f476df9ec4cfe1ff06e
hayato hasegawa <58217100+y-i-t-m@users.noreply.github.com>
2020-05-18 15:23:38 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import _ from 'lodash';
+// import { name as name_of_hase } from "./utilities";
+import Tiger from "./utilities";
+
+console.log(Tiger.say());
+// console.log(nijou(5));
+// console.log(name_of_hase);
 
 function component() {
   const element = document.createElement('div');
---
+++ src/utilities.js
@@ -0,0 +1,11 @@
+export function nijou(num) {
+  return num ** 2;
+}
+
+export const name = "はせ";
+
+export default class Lion {
+  static say() {
+    return "Roar";
+  }
+}
